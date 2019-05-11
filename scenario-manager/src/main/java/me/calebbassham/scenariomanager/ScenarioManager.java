a846542ff3cd39efcd902341b9013d90138e3d68
Caleb Bassham <caleb.bassham@gmail.com>
2019-05-11 10:34:09 -0500

    Add method to get all registered scenarios

--- scenario-manager/src/main/java/me/calebbassham/scenariomanager/ScenarioManager.java
+++ scenario-manager/src/main/java/me/calebbassham/scenariomanager/ScenarioManager.java
@@ -6,6 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
 
@@ -77,6 +78,10 @@ public final class ScenarioManager implements Listener {
 
     public static boolean isGameRunning() {
         return gameRunning;
+    }
+
+    public static Collection<Scenario> getScenarios() {
+        return scenarios.values();
     }
 
     @EventHandler
